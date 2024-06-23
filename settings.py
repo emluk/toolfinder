cd109885e69fcda91769e2b7722c1245a3f35b16
emluk <86768844+emluk@users.noreply.github.com>
2024-06-23 13:50:35 +0200

    chore: Remove absolute path

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import configparser
 import DataBase.DataAccess.SQLiteConnector
-#from DataAccess.SQLiteConnector import SQLiteConnector
+
 
 config = configparser.ConfigParser()
-config.read("D:\\Priv\\repository\\BA-Code\\toolfinder.ini")
+config.read(".\\toolfinder.ini")
 
 db_connection = DataBase.DataAccess.SQLiteConnector.SQLiteConnector(config['db']['path'])
 db_connection.connect()
